81c3ecede99bfd69a6d4bacc0cff36b704820065
kazii <kazdan@seznam.cz>
2023-12-09 19:09:32 +0100

    Fixed folder structure

---
+++ src/app/components/Intro/IntroFirst.tsx
@@ -0,0 +1,11 @@
+const IntroFirst = () => {
+    return (
+        <div className="flex flex-col items-center justify-center w-full h-full">
+            <div>
+                Intro first
+            </div>
+        </div>
+    )
+}
+
+export default IntroFirst
--- src/app/components/Intro/IntroLogin.tsx
+++ src/app/components/Intro/IntroLogin.tsx
@@ -1,16 +1,38 @@
-
+'use client'
+import { useState } from "react"
 
 const IntroLogin = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [signType, setSignType] = useState('email') 
 
       
     return (
-        <div className="flex flex-col w-full h-full items-center justify-center gap-20">
-            <div className="fullbox">
-                By setting a habbit 5 hours / week you will gain 250h+ expertize at any skill, feel physically better, 
+        <div className="flex flex-col justify-center items-center  h-full">
+            <div className="flex flex-col w-[90%] gap-5
+                rounded-lg
+            ">
+                <div className="fullbox">
+                    By setting a habbit 5 hours / week you will gain 250h+ expertize at any skill, feel physically better, 
+                </div>
+                {signType === 'email' && <div>
+                    <input className="fullbox" type="text" placeholder="Username" onChange={e => setUsername(e.target.value)}/> 
+                    <input className="fullbox" type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
+                </div>}
+                {signType === 'google' && <div>
+                    <button className="fullbox">Sign in with Google</button>
+                </div>
+                }
+                
+               <div className="flex flex-row justify-center bg-gray-950 gap-3">
+                <button 
+                    className=" p-4 text-main border border-transmain rounded-2xl" 
+                    onClick={() => setSignType('google')}>G</button>
+                <button 
+                    className="p-4 text-main border border-transmain rounded-2xl" 
+                    onClick={() => setSignType('email')}>M</button>
+                </div>
             </div>
-            <button className="
-                bg-gray-950 
-            ">Sign in with Google</button>
         </div>
     )
 }
