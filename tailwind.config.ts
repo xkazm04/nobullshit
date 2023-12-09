9bdf518a1c5817957e9134b1834abdbd9fd12a59
kazii <kazdan@seznam.cz>
2023-12-09 13:06:42 +0100

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,14 +7,41 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: '0' },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: '0' },
+        },
+        'pulse-move': {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0%)' }
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+        'pulse-move': 'pulse-move 2s',
+      },
     },
   },
-  plugins: [],
+  darkMode: ["class"],
+  plugins: [require("tailwindcss-animate")],
 }
 export default config
