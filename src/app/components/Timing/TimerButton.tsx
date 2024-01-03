e391593fe4b215a7bfb60d3c7a5fa7e14327a5d5
kazii <kazdan@seznam.cz>
2024-01-03 22:46:55 +0100

    Timer UI logic

--- src/app/components/Timing/TimerButton.tsx
+++ src/app/components/Timing/TimerButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { getCategoryColor } from '@/app/lib/colorGetter';
 import { motion, AnimatePresence } from 'framer-motion';
+import { CheckCheckIcon } from 'lucide-react';
 import { useState } from 'react';
 
 
@@ -23,6 +24,9 @@ function hexToRgb(hex: string): { r: number, g: number, b: number } | "null" {
 }
 
 const TimerButton: React.FC<TimerButtonProps> = ({ category, time, habitId, task }) => {
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+    const [completed, setCompleted] = useState(false)
+    const [timer, setTimer] = useState(time);
     const hexColor = getCategoryColor(category);
     const rgbColor = hexToRgb(hexColor);
     const borderColor = hexColor;
@@ -34,32 +38,53 @@ const TimerButton: React.FC<TimerButtonProps> = ({ category, time, habitId, task
     const [active, setActive] = useState(false);
     const startTimer = () => {
         setActive(true)
+        let id = setInterval(() => {
+            setTimer(timer => timer - 1)
+        }, 1000)
+        setIntervalId(id);
+        setTimeout(() => {
+            setCompleted(true);
+            stopTimer();
+        }, time * 1000)
     }
 
     const stopTimer = () => {
         setActive(false)
+        if (intervalId) {
+            clearInterval(intervalId);
+            setIntervalId(null);
+        }
+    }
+
+    const save = () => {
+        console.log('save')
     }
     return (
         <div>
             <AnimatePresence>
                 {!active ?
                     <motion.button
-                        className={`text-xs px-3 py-2 bg-gray-950 rounded-xl border w-[100px] h-[100px]
-                animate-fadeIn transition-all duration-500 ease-in-out`}
+                        className={`text-xs px-3 py-2 rounded-xl border w-[100px] h-[100px]
+        animate-fadeIn transition-all duration-500 ease-in-out ${completed ? 'bg-green-500' : 'bg-gray-950'}`}
                         style={{ borderColor: borderColor }}
-                        onClick={() => startTimer()}>
+                        onClick={() => !completed ? startTimer() : save()}>
                         <div>
-                            <div className='flex justify-center'><div className='text-lg'>{time}</div><div>s</div></div>
+                            <div className='flex justify-center'>
+                                <div className='text-lg'>
+                                    {!completed ? <>{timer}</> : <CheckCheckIcon />}
+                                </div>
+                                <div>{!completed ? <>s</> : <>save</>}</div>
+                            </div>
                             <div className='font-mono text-xs'>{task}</div>
                         </div>
                     </motion.button> :
                     <motion.button
-                        className={`text-xs px-3 py-2 bg-green-500/20 rounded-xl border w-[130px] h-[130px]
+                        className={`text-xs px-3 py-2 bg-green-500/10 rounded-xl border w-[140px] h-[140px]
             animate-fadeIn transition-all duration-500 ease-in-out`}
                         style={{ borderColor: borderColor, backgroundColor: backgroundColor }}
                         onClick={() => stopTimer()}>
                         <div>
-                            <div className='flex justify-center'><div className='text-2xl'>{time}</div><div>s</div></div>
+                            <div className='flex justify-center'><div className='text-2xl'>{timer}</div><div>s</div></div>
                             <div className='font-mono text-xs'>{task}</div>
                         </div>
                     </motion.button>}
